96a3b5d319f70d77e8777abf62ff6d9f0eeda43b
ashzhang1 <ashzhang1@gmail.com>
2023-08-05 23:20:57 +1000

    seperated output texts into different file

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
+import * as cmd_outputs from './command_outputs.js';
+
 var ascii_banner_element = document.getElementById('ascii-banner');
 var output_area_element = document.getElementById('Output-area');
 
-const asciiText = String.raw`
-/$$$$$$            /$$       /$$                           /$$$$$$$$ /$$                                    
-/$$__  $$          | $$      | $$                          |_____ $$ | $$                                    
-| $$  \ $$  /$$$$$$$| $$$$$$$ | $$  /$$$$$$  /$$   /$$           /$$/ | $$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$ 
-| $$$$$$$$ /$$_____/| $$__  $$| $$ /$$__  $$| $$  | $$          /$$/  | $$__  $$ |____  $$| $$__  $$ /$$__  $$
-| $$__  $$|  $$$$$$ | $$  \ $$| $$| $$$$$$$$| $$  | $$         /$$/   | $$  \ $$  /$$$$$$$| $$  \ $$| $$  \ $$
-| $$  | $$ \____  $$| $$  | $$| $$| $$_____/| $$  | $$        /$$/    | $$  | $$ /$$__  $$| $$  | $$| $$  | $$
-| $$  | $$ /$$$$$$$/| $$  | $$| $$|  $$$$$$$|  $$$$$$$       /$$$$$$$$| $$  | $$|  $$$$$$$| $$  | $$|  $$$$$$$
-|__/  |__/|_______/ |__/  |__/|__/ \_______/ \____  $$      |________/|__/  |__/ \_______/|__/  |__/ \____  $$
-                                            /$$  | $$                                               /$$  \ $$
-                                            |  $$$$$$/                                              |  $$$$$$/
-                                            \______/                                                \______/
-`;
+ascii_banner_element.textContent = cmd_outputs.asciiText;
 
-
-ascii_banner_element.textContent = asciiText;
 document.getElementById("command-line").addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         var cleaned_content = document.getElementById('command-line').value.replace(/\n/g, '');
         command_controller(cleaned_content);
     }
 });
-
 
 function clearOutputArea() {
     document.getElementById('command-line').value = '';
@@ -48,7 +35,6 @@ function submitText() {
     output_area_element.appendChild(new_element);
     output_area_element.append(linebreak);
 }
-
 
 function formatOutputText(text) {
     return 'visitor@ashley_zhang:~% ' + text;
